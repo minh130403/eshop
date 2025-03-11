c13b04bf624d0821d2260e856439819f2683a45f
minh130403 <tienminh130403@gmail.com>
2025-03-12 06:03:38 +0700

    add tinymce

--- resources/views/admin/dashbroad.blade.php
+++ resources/views/admin/dashbroad.blade.php
@@ -10,14 +10,10 @@
 
     {{-- Fontanwsome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.tiny.cloud/1/q621zku9ks0ovuoai50q1h5mksnfw5iyec9hnvib6xrbgefr/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
     @vite(['resources/js/admin.js', 'resources/js/bootstrap.js', 'resources/css/bootstrap.css', 
     'resources/css/admin.css'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
-   <style>
-
-    </style>
-   
+    <script src="{{ asset('tinymce/js/tinymce/tinymce.min.js') }}"></script>
 </head>
 <body>
    <div class="">
